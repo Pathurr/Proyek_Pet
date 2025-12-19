0b9530ba638f1cfd0832c7183268eab18d6f5900
Fathur <alfathurahman28@gmail.com>
2025-12-19 22:17:57 +0700

    Initial Commit

--- app/src/main/java/com/example/proyek/MainActivity.kt
+++ app/src/main/java/com/example/proyek/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
-
+    //testing
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
