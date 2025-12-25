712077291b8f251c7571b0d277905699261a3cbe
Fathur <alfathurahman28@gmail.com>
2025-12-26 03:21:06 +0700

    Initial Commit

--- app/src/main/java/com/example/proyek/HomeActivity.kt
+++ app/src/main/java/com/example/proyek/HomeActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-//sunda
+//su
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
